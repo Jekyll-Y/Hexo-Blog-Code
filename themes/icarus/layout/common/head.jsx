91093fe006a7990b27e8448f9691ba98d8f7f7cb
Jekyll_Y <73386099+Jekyll-Y@users.noreply.github.com>
2022-11-03 19:50:18 +0800

    themes

--- themes/icarus/layout/common/head.jsx
+++ themes/icarus/layout/common/head.jsx
@@ -169,6 +169,7 @@ module.exports = class extends Component {
             {hlTheme ? <link rel="stylesheet" href={cdn('highlight.js', '9.12.0', 'styles/' + hlTheme + '.css')} /> : null}
             <link rel="stylesheet" href={fontCssUrl[variant]} />
             <link rel="stylesheet" href={url_for('/css/' + variant + '.css')} />
+            <link rel="dns-prefetch" href="https://jekyll-y.github.io/" />
             <Plugins site={site} config={config} helper={helper} page={page} head={true} />
 
             {adsenseClientId ? <script data-ad-client={adsenseClientId}
