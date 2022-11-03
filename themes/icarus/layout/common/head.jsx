30c1b9967e12c18c9277034ffa414da04ceda810
Jekyll_Y <73386099+Jekyll-Y@users.noreply.github.com>
2022-11-03 19:39:38 +0800

    themes

--- themes/icarus/layout/common/head.jsx
+++ themes/icarus/layout/common/head.jsx
@@ -53,7 +53,7 @@ module.exports = class extends Component {
 
         const language = page.lang || page.language || config.language;
         const fontCssUrl = {
-            default: fontcdn('Ubuntu:wght@400;600&family=Source+Code+Pro', 'css2'),
+            default: fontcdn('Ubuntu:wght@400;600&family=Fira+Sans', 'css2'),
             cyberpunk: fontcdn('Oxanium:wght@300;400;600&family=Roboto+Mono', 'css2')
         };
 
